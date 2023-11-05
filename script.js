c83f5b7eb8e60e391e62a91f33ea878bd58cd78f
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-11-05 12:38:53 -0800

    Back to rubric, rework all of js to include specifics as to assignment;  from online class code to assignment requirements.  Thorough messaging.

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-        const shoppingList = document.getElementById("shoppingList");
-    var input = document.getElementById("item");
-    const addBtn = document.getElementById("addBtn");
-
-    addBtn.addEventListener("click", function() {
-        additemToList();
-    });
-
-    input.addEventListener("keypress", function(e) {
-        if (e.key === 'Enter') {
-            additemToList();
-        }
-    });
-     
-  function additemToList() {
+//target shoppingList//
+document.addEventListener("DOMContentLoaded", function (e) {
+  const shoppingList = document.querySelector("#shoppingList");
+  //target #form input//
+ const input = document.querySelector("#item");
+  const addBtn = document.querySelector("#addBtn");
+  //add button with click event//
+  addBtn.addEventListener("click", function (e) {
+    additemToList();
+  });
+  //add button with enter keypress event//
+  document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      additemToList();
+    }
+  });
+  //when adding item to list: add button, span, and li//
+  function additemToList(e) {
     const li = document.createElement("li");
+    //delete button//
     const button = document.createElement("button");
+    //button text//
     button.textContent = "Delete";
-    button.addEventListener("click", function(e) {
+    //delete button with click event//
+    button.addEventListener("click", function (e) {
       shoppingList.removeChild(li);
     });
     li.appendChild(button);
-    const textNode = document.createTextNode(input.value);
-    li.appendChild(textNode);
+   
+
+    //textNode instead of span//
+    const span = document.createElement("span");
+    span.textContent = input.value;
+    li.appendChild(span);
     shoppingList.appendChild(li);
     input.value = "";
     input.focus();
-  } 
-       
-    });
+  }
+});
